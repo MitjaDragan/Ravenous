a9e69f9686141c5894c9650a73f73a76d0d8c788
MitjaDragan <mitja.dragan@gmail.com>
2024-08-08 14:15:59 +0200

    Adding search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
 
 const mockBusinesses = [
   {
-      imageSrc: 'https://via.placeholder.com/150',
-      name: 'MarginOtto Pizzeria',
-      address: '1010 Paddington Way',
-      city: 'Flavortown',
-      state: 'NY',
-      zipCode: '10101',
-      category: 'Italian',
-      rating: 4.5,
-      reviewCount: 90
+    imageSrc: 'https://via.placeholder.com/150',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
   },
   {
-      imageSrc: 'https://via.placeholder.com/150',
-      name: 'The Taco Place',
-      address: '123 Taco Lane',
-      city: 'Taco Town',
-      state: 'TX',
-      zipCode: '78945',
-      category: 'Mexican',
-      rating: 4.7,
-      reviewCount: 120
+    imageSrc: 'https://via.placeholder.com/150',
+    name: 'The Taco Place',
+    address: '123 Taco Lane',
+    city: 'Taco Town',
+    state: 'TX',
+    zipCode: '78945',
+    category: 'Mexican',
+    rating: 4.7,
+    reviewCount: 120
   },
   {
-      imageSrc: 'https://via.placeholder.com/150',
-      name: 'Burger Bonanza',
-      address: '456 Burger Blvd',
-      city: 'Burger City',
-      state: 'CA',
-      zipCode: '90210',
-      category: 'American',
-      rating: 4.3,
-      reviewCount: 75
-  },
-  // Add more businesses as needed
+    imageSrc: 'https://via.placeholder.com/150',
+    name: 'Burger Bonanza',
+    address: '456 Burger Blvd',
+    city: 'Burger City',
+    state: 'CA',
+    zipCode: '90210',
+    category: 'American',
+    rating: 4.3,
+    reviewCount: 75
+  }
 ];
 
 function App() {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,11 +8,46 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        };
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSortByChange = this.handleSortByChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    handleLocationChange(event) {
+        this.setState({ location: event.target.value });
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({ sortBy: sortByOption });
+    }
+
+    handleSearch(event) {
+        event.preventDefault();
+        console.log(`Searching Yelp with ${this.state.term}, ${this.state.location}, ${this.state.sortBy}`);
+    }
+
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
             return (
-                <li key={sortByOptionValue}>
+                <li
+                    key={sortByOptionValue}
+                    className={this.state.sortBy === sortByOptionValue ? 'active' : ''}
+                    onClick={() => this.handleSortByChange(sortByOptionValue)}
+                >
                     {sortByOption}
                 </li>
             );
@@ -28,11 +63,19 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input
+                        placeholder="Search Businesses"
+                        value={this.state.term}
+                        onChange={this.handleTermChange}
+                    />
+                    <input
+                        placeholder="Where?"
+                        value={this.state.location}
+                        onChange={this.handleLocationChange}
+                    />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         );
