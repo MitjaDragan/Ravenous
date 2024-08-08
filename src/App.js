5d3f2d99f4bff8cd798ebb29ad5df2d3b88c3c10
MitjaDragan <mitja.dragan@gmail.com>
2024-08-08 13:08:49 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BusinessList from './components/BusinessList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ravenous</h1>
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
